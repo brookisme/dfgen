a9535539c7019ba357a3ce98aec21063edd688c8
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-07-02 21:38:12 -0700

    underscore default. tags_to_labels_column to emphasize role. always set self.tags. self.csv_sep.  save_df.  _default.  thoughts on restrict

--- dfgen.py
+++ dfgen.py
@@ -7,10 +7,12 @@ import numpy as np
 CSV_SEP=' '
 CONFIG_FILE_NAME='dfg_config.yaml'
 PATH_COLUMN='dfg_paths'
+ERROR_REQUIRED_COLUMNS='ERROR[DFGen]: both image and label column are requrired.'
 
 class DFGen():
     """ CREATES GENERATOR FROM DATAFRAME
-    
+        * load df directly from a df or from a csv
+        * ...
     """
     def __init__(
             self,
@@ -18,7 +20,7 @@ class DFGen():
             dataframe=None,
             image_column=None,
             label_column=None,
-            tag_column=None,
+            tags_to_labels_column=None,
             tags=None,
             batch_size=None,
             image_dir=None,
@@ -29,12 +31,35 @@ class DFGen():
         self._load_defaults()
         self._set_dataframe(csv_file,dataframe,csv_sep)
         self._set_columns(image_column,label_column)
-        self._set_tags(tags,tag_column)
+        self._set_tags(tags,tags_to_labels_column)
         self._set_paths_and_labels()
         self._set_image_dir_and_ext(image_dir,image_ext)
-        self.batch_size=batch_size or self.default('batch_size')
+        self.batch_size=batch_size or self._default('batch_size')
         self.lambda_func=lambda_func
         self.batch_index=0
+
+
+    def save_df(self,path):
+        """
+            save processed-dataframe
+            once saved can pass without 
+            tags_to_labels_column
+        """
+        self.dataframe.to_csv(path,sep=self.csv_sep)
+
+
+    def restrict(self,pct_list):
+        """
+            Args:
+                pct_list: 
+                    list corresponding to labels vector
+                    on what percentage of each column to keep.
+                    - int (% to keep)
+                    - '*' keep any percentage
+                    - '!' remove
+            ??? what about 50% blowdown 50% not blowdown
+        """
+        pass
 
 
     def __next__(self):
@@ -70,6 +95,11 @@ class DFGen():
         self._defaults={}
 
 
+    def _default(self,key):
+        """ safe get default
+        """
+        return self._defaults.get(key,None)
+
 
     def _img_data(self,path):
         """Read Data 
@@ -93,15 +123,15 @@ class DFGen():
                 - else: try default by ext and image_dirs
                 - or: default
         """
-        self.image_ext=image_ext or self.default('image_ext')
+        self.image_ext=image_ext or self._default('image_ext')
         if image_dir: self.image_dir=image_dir
         else:
             if self.image_ext:
-                image_dirs=self.default('image_dirs')
+                image_dirs=self._default('image_dirs')
                 if image_dirs: 
                     self.image_dir=image_dirs.get(self.image_ext)
             if not self.image_dir:
-                self.image_dir=self.default('image_dir')
+                self.image_dir=self._default('image_dir')
 
 
     def _set_dataframe(self,file_path,df,csv_sep):
@@ -112,17 +142,21 @@ class DFGen():
                 self.dataframe
             the paths and labels are pairwised shuffled
         """
+        self.csv_sep=csv_sep or self._default('csv_sep') or CSV_SEP
         if file_path: 
-            csv_sep=csv_sep or self.default('csv_sep') or CSV_SEP
-            df=pd.read_csv(self.file_path,sep=csv_sep)
+            df=pd.read_csv(self.file_path,sep=self.csv_sep)
         self.size=df.shape[0]
         self.dataframe=df
 
 
 
     def _set_columns(self,image_column,label_column):
-        self.image_column=image_column or self.default('image_column')
-        self.label_column=label_column or self.default('label_column')
+        """ set image and label column
+        """
+        self.image_column=image_column or self._default('image_column')
+        self.label_column=label_column or self._default('label_column')
+        if not (self.image_column and self.label_column):
+            raise ValueError(ERROR_REQUIRED_COLUMNS)
 
 
 
@@ -130,10 +164,10 @@ class DFGen():
         """
             if tags and tags column:
                 * set tag properties
-                * create label column from tags
+                * if tags_column: create label column from tags
         """
-        if tags and tag_column:
-            self.tags=tags
+        self.tags=tags
+        if tag_column:
             self.tag_column=tag_column
             self.dataframe[self.label_column]=self.dataframe[self.tags_column].apply(
                 self._tags_to_vec)
